d4a9c448576d82266202c902c2fdad68be674caa
s316379 <s316379@studenti.polito.it>
2024-05-14 19:21:22 +0200

    cmd line argument correction

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -29,16 +29,16 @@ ckpt_path="/path/to/checkpoints/model.pt"
 
 python -m torch.distributed.launch --nproc_per_node=1 main.py \
     --model $model \
-    --batch_size 32 \
+    --batch-size 32 \
     --npoints 10000 \
-    --num_group 512 \
-    --group_size 64 \
-    --pc_encoder_dim 512 \
-    --clip_model $clip_model \
+    --num-group 512 \
+    --group-size 64 \
+    --pc-encoder-dim 512 \
+    --clip-model $clip_model \
     --pretrained $pretrained \
-    --pc_model $pc_model \
-    --pc_feat_dim $pc_feat_dim \
-    --embed_dim 1024 \
+    --pc-model $pc_model \
+    --pc-feat-dim $pc_feat_dim \
+    --embed-dim 1024 \
     --validate_dataset_name modelnet40_openshape \
     --validate_dataset_name_lvis objaverse_lvis_openshape \
     --validate_dataset_name_scanobjnn scanobjnn_openshape \
